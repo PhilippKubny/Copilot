e3b2c31f25e019c0332892940ebd4673806bcef8
m4nu-el <manuel.elia@t-online.de>
2024-04-03 20:52:45 +0200

    update langchain_test

--- backend/api/langchain_test.py
+++ backend/api/langchain_test.py
@@ -1,6 +1,8 @@
 from langchain_openai import AzureOpenAI
 from langchain_openai import AzureChatOpenAI
 from langchain.schema import HumanMessage
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
 import os
 import openai
 from dotenv import load_dotenv
@@ -15,12 +17,22 @@ llm = AzureChatOpenAI(
     azure_deployment = os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME")
 )
 
-# Define the prompt we want the AI to respond to - the message the Human user is asking
-msg = HumanMessage(content="Explain step by step. How old is the president of USA?")
+# # Define the prompt we want the AI to respond to - the message the Human user is asking
+# msg = HumanMessage(content="Explain step by step. How old is the president of USA?")
 
-# Call the API
-r = llm.invoke([msg])
+# # Call the API
+# r = llm.invoke([msg])
 
-# Print the response
-print(r.content)
+# # Print the response
+# print(r.content)
 
+promt = PromptTemplate(
+    input_variables=['input'],
+    template='What interesting things can I make with a {input}?'
+)
+
+chain = LLMChain(llm=llm, prompt=promt)
+
+response = chain.invoke({'input': 'rasperry pi'})
+
+print(response['text'])
